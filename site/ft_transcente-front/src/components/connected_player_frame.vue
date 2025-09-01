a699428d3e08e8cb7a19119d5e1bc17837b8a990
Draask <manegoste@gmail.com>
2025-09-01 15:28:05 +0200

    early add frame player connect

---
+++ site/ft_transcente-front/src/components/connected_player_frame.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<div	tittle="connected_player_frame" class="frame">
+		<div>
+		<img src="../../images/default_avatar.png" alt="Avatar" class="avatar">
+		</div>
+		<div tittle="login" class="login">
+			<label>Login</label>
+		</div>
+		<div tittle="nbr-game" class="stat">
+			<label>20</label>
+		</div>
+		<div tittle="nbr-victory" class="stat">
+			<label>15</label>
+		</div>
+		<div tittle="ratio" class="stat">
+			<label>19.09</label>
+		</div>
+		<div tittle="Rank" class="stat">
+			<label>1</label>
+		</div>
+	</div>
+</template>
+
+<style>
+@font-face {
+	font-family: "netron";
+	src: url("../../fonts/netron.regular.otf") format("opentype");
+	}
+
+	.frame{
+		display: flex;
+		justify-content: space-between;
+		align-items: flex-start;
+		width: 80rem;
+		background-color: rgba(156, 50, 133, 0.5);
+		border: 2px solid #e251ca;
+		box-shadow: 
+		0 0 5px #dd0aba,
+		0 0 10px #dd0aba,
+		0 0 20px #dd0aba,
+		0 0 40px #dd0aba;
+		padding: 2rem 3rem;
+		border-radius: 20px;
+		transition:  background-color 0.3s ease, box-shadow 0.3s ease-in-out, text-shadow 0.3s ease-in-out, border 0.3s ease-in-out;
+		margin-top: 3rem
+	}
+
+	.avatar{
+		width: 10rem;
+		height: 10rem;
+		border-radius: 50%;
+	}
+
+	.login{
+		display: flex;
+		align-self: center;
+		justify-content: center;
+		font-family: netron;
+		color: white;
+		text-shadow: 
+		0 0 10px #dd0aba,
+		0 0 10px #dd0aba,
+		0 0 20px #dd0aba,
+		0 0 40px #dd0aba,
+		0 0 80px #ff69b4,
+		0 0 120px #dd0aba;
+		font-size: 3rem;
+		font-family: netron;
+		color: white;
+		text-shadow: 
+		0 0 10px #dd0aba,
+		0 0 10px #dd0aba,
+		0 0 20px #dd0aba,
+		0 0 40px #dd0aba,
+		0 0 80px #ff69b4,
+		0 0 120px #dd0aba;
+		margin-bottom: 1rem;
+	}
+</style>
--- site/ft_transcente-front/src/views/HomeView.vue
+++ site/ft_transcente-front/src/views/HomeView.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
+	import con_pla_fra from '../components/connected_player_frame.vue'
 	import Head from '../components/Header.vue';
 	import Connexion from '../components/ConnexionButton.vue';
 	import connection_form from '../components/connection_form.vue';
 	import Signup from '@/components/Signup.vue';
-	import { ref } from 'vue';
+	import { ref, watch, onUnmounted, nextTick,computed } from 'vue';
 
 	const props = defineProps<{
 			setLanguage: (lang: string) => void;
 		}>();
+
+	const isConnect = ref(false)
 
 	const showSignup = ref(false);
 	const toggleSignup = () => {
@@ -21,6 +24,46 @@
 		showConnection.value = !showConnection.value;
 	}
 
+	const connectionBox = ref<HTMLElement | null>(null);
+	const signUpbox =  ref<HTMLElement | null>(null);
+	const handlePointerDownOutside = (e: PointerEvent) => {
+		const target = e.target as Node;
+		if(connectionBox.value && !connectionBox.value.contains(target))
+			{
+				showConnection.value = false;
+			}
+		if(signUpbox.value && !signUpbox.value.contains(target))
+			{
+				showSignup.value = false;
+			}
+	}
+
+	const anyOpen = computed(() => showConnection.value || showSignup.value);
+	watch(anyOpen, (newValue) => {
+		if (newValue) {
+			nextTick(() => {
+				document.addEventListener(
+					'pointerdown',
+					handlePointerDownOutside,
+					{ capture: true}
+				);
+			})
+		}	
+		else {
+			document.removeEventListener(
+				'pointerdown',
+				handlePointerDownOutside,
+				{ capture: true}
+			);
+		}
+	});
+
+	onUnmounted(() => {
+  document.removeEventListener('pointerdown', handlePointerDownOutside, { capture: true });
+});
+
+
+
 </script>
 
 
@@ -30,22 +73,43 @@
 		<Head :setLanguage="props.setLanguage" @show-form="toggleSignup"></Head>
 		</div>
 		<div>
-			<div class="pos-con-but">
-				<Connexion :setLanguage="props.setLanguage" v-show="!showSignup && !showConnection" @show-connection="toggleConnection"></Connexion>
-				<Signup :setLanguage="props.setLanguage" v-show="showSignup"></Signup>
-				<connection_form :setLanguage="props.setLanguage" v-show="showConnection && !showSignup"></connection_form>
+			<div v-show="isConnect" tittle="home_disconnect" class="home_disconnect" >
+				<div>
+					<Connexion :setLanguage="props.setLanguage" v-show="!showSignup && !showConnection" @show-connection="toggleConnection"></Connexion>
+				</div>
+				<div ref="signUpbox">
+					<Signup :setLanguage="props.setLanguage" v-show="showSignup"></Signup>
+				</div>
+				<div ref="connectionBox">
+					<connection_form :setLanguage="props.setLanguage" v-show="showConnection && !showSignup"></connection_form>
+				</div>
+			</div>
+			<div tittle="home_connect" class="home_connect" >
+				<div>
+					<con_pla_fra></con_pla_fra>
+				</div>
 			</div>
 		</div>
 	</div>
 </template>
 
 <style>
-	.pos-con-but{
+
+	.home_disconnect{
 		display: flex;
 		justify-content: center;
 		position: absolute;
 		top: 50%;
 		left: 50%;
 		transform: translate(-50%, -50%);
+	}
+
+	.home_connect{
+		display: flex;
+		justify-content: center;
+		/*position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);*/
 	}
 </style>
