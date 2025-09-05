a31ec6f0f721db754038aeacf5846510f30a46f0
Louise Collonge <lcollong@c2r11p2.42lehavre.fr>
2025-09-05 08:22:26 +0200

    documentation, package.json, une database d'exemple, et tous les fichiers node.js et vue

---
+++ documentation.sh
@@ -0,0 +1,63 @@
+# Ce fichier contient des exemples de commandes pour manipuler la base de donnees. 
+# Il ne faut pas executer ce fichier, il ne sert que de documentation pour se rappeler de ces commandes. 
+
+
+# installer node.js et fastify:
+npm init -y
+npm install fastify fastify-plugin sqlite3
+
+
+# lancer le serveur:
+node db_server.js
+
+
+# Dans un autre terminal:
+
+	# ajouter un user:
+	curl -X POST http://localhost:3001/users \
+		-H "Content-Type: application/json" \
+		-d '{"login": "Louise", "email": "louise@example.com", "password": "bimo"}' \
+
+	curl -X POST http://localhost:3001/users \
+		-H "Content-Type: application/json" \
+		-d '{"login": "Alice", "email": "alice@example.com", "password": "bimo"}' \
+
+	curl -X POST http://localhost:3001/users \
+		-H "Content-Type: application/json" \
+		-d '{"login": "Cocotte", "email": "cocotte@example.com", "password": "zoom"}'
+
+	# afficher tous les users:
+	curl http://localhost:3001/users
+
+	# ajouter une partie:
+	curl -X POST http://localhost:3001/games \
+		-H "Content-Type: application/json" \
+		-d '{"login_winner": "Louise", "login_loser": "Alice", "score_winner": "6", "score_loser": "4"}' \
+
+	curl -X POST http://localhost:3001/games \
+		-H "Content-Type: application/json" \
+		-d '{"login_winner": "Cocotte", "login_loser": "Alice", "score_winner": "3", "score_loser": "0"}' \
+
+	curl -X POST http://localhost:3001/games \
+		-H "Content-Type: application/json" \
+		-d '{"login_winner": "Cocotte", "login_loser": "Louise", "score_winner": "5", "score_loser": "4"}'
+
+	# afficher toutes les parties:
+	curl http://localhost:3001/games
+
+	# afficher les amis:
+	curl http://localhost:3001/friends
+
+	# ajouter un ami a qqn:
+	curl -X POST http://localhost:3001/friends \
+		-H "Content-Type: application/json" \
+		-d '{"login1": "Alice", "login2": "Cocotte"}' \
+
+	curl -X POST http://localhost:3001/friends \
+		-H "Content-Type: application/json" \
+		-d '{"login1": "Cocotte", "login2": "Louise"}'
+
+	# supprimer une amitie:
+	curl -X POST http://localhost:3001/friends/delete \
+		-H "Content-Type: application/json" \
+		-d '{"login1": "Cocotte", "login2": "Louise"}'
