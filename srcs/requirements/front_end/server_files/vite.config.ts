855ae8da6efbf188dcfff2fb92b68a568a5ad466
ahoizai <ahoizai@Inception42.myguest.virtualbox.org>
2025-09-26 11:41:16 +0200

    Setup: Https configured in dev OK

--- srcs/requirements/front_end/server_files/vite.config.ts
+++ srcs/requirements/front_end/server_files/vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,10 @@ export default defineConfig({
     vueDevTools(),
   ],
   server: {
+    https: {
+    key: fs.readFileSync('./certs/hgp_https.key'),
+    cert: fs.readFileSync('./certs/hgp_https.crt'),
+  },
     proxy: {
       '/api': {
         target: 'http://hgp_user_management:3000',
@@ -19,7 +24,7 @@ export default defineConfig({
         ws: true,
       },
     },
-    host: '0.0.0.0', 
+    host: '0.0.0.0',
     port: 5173,
   },
   resolve: {
--- srcs/requirements/user_management/server_files/db_server.js
+++ srcs/requirements/user_management/server_files/db_server.js
@@ -130,6 +130,8 @@ fastify.post('/upload-avatar', async (request, reply) => {
 	const allowedOrigins = [
 		'http://localhost:5173',
 		'http://127.0.0.1:5173',
+		'https://localhost:5173',
+		'https://127.0.0.1:5173',
 	];
 
 	await fastify.register(cors, {
