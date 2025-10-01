f5bed8605031c7551988bd226e6461106079557a
Axel Hoizai <ahoizai@c1r1p10.42lehavre.fr>
2025-10-01 15:19:43 +0200

    fix: addFriends (empty str, fake friends, same friends)

--- srcs/requirements/front_end/server_files/src/components/connected_home_view/friendlist.vue
+++ srcs/requirements/front_end/server_files/src/components/connected_home_view/friendlist.vue
@@ -96,7 +96,7 @@ async function addFriend() {
 			})
 		});
 		console.log(result);
-		if (!result.ok || !result.statusText) {
+		if (!result.ok || result.status == 304 || result.status == 204) {
 			search_friends.value = "";
 			return console.log(`Erreur d'ajout d'amis`);
 		}
